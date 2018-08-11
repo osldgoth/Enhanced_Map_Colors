2ae33914ad4b1e88443c4c12aa53cf553d0eed5c
U-Spire\Osldgoth <osldgoth@gmail.com>
2018-08-10 20:13:14 -0400

    added colors for heatpipes and reactors

--- data-updates.lua
+++ data-updates.lua
@@ -16,6 +16,9 @@ data.raw["pipe"]["pipe"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56} -- 
 data.raw["pipe-to-ground"]["pipe-to-ground"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56}
 data.raw["storage-tank"]["storage-tank"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56}
 
+data.raw["heat-pipe"]["heat-pipe"].friendly_map_color = {r = 0.56, g = 0.0, b = 0.0} -- 142, 0, 0
+data.raw["reactor"]["nuclear-reactor"].friendly_map_color = {r = 0.16, g = 0.73, b = 0.15} -- 41, 186, 37
+
 --data.raw["mining-drill"]["burner-mining-drill"].map_color = {r = 0.0, g = 0.37, b = 0.08} -- 0, 95, 20
 --data.raw["mining-drill"]["electric-mining-drill"].map_color = {r = 0.0, g = 0.37, b = 0.08} -- 74, 23, 143
 --Bob's Belts
@@ -33,31 +36,31 @@ end
 
 --5dim belts
 if data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-30"] then
-	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-30"].map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
-	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-30"].map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
-	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-30"].map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
+	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-30"].friendly_map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
+	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-30"].friendly_map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
+	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-30"].friendly_map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
 	
-	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-50"].map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
-	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-50"].map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
-	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-50"].map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
+	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-50"].friendly_map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
+	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-50"].friendly_map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
+	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-50"].friendly_map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
 end
 
 if data.raw["transport-belt"]["5d-mk4-transport-belt"] ~= nil then
 	data.raw["transport-belt"]["5d-mk4-transport-belt"].friendly_map_color = {r = 0.08, g = 0.66, b = 0.14} -- 20, 168, 36
 	data.raw["splitter"]["5d-mk4-splitter"].friendly_map_color = {r = 0.06, g = 0.53, b = 0.11} -- 16, 134, 29
 	
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground"].map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-30"].map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-50"].map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
+	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
+	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-30"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
+	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-50"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
 end
 
 if data.raw["transport-belt"]["5d-mk5-transport-belt"] ~= nil then
 	data.raw["transport-belt"]["5d-mk5-transport-belt"].friendly_map_color = {r = 0.89, g = 0.91, b = 0.96} -- 227, 232, 245
 	data.raw["splitter"]["5d-mk5-splitter"].friendly_map_color = {r = 0.71, g = 0.73, b = 0.77} -- 182, 186, 196
 	
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground"].map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-30"].map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-50"].map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
+	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
+	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-30"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
+	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-50"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
 end
 
 --[[
@@ -90,12 +93,12 @@ end
 
 --Electric Poles including those in Bob's mods and 5dim
 for _, v in pairs(data.raw["electric-pole"]) do
-	v.map_color = {r = 0.65, g = 0.65, b = 0.65} -- 166, 166, 166
+	v.friendly_map_color = {r = 0.65, g = 0.65, b = 0.65} -- 166, 166, 166
 end
 
 --Steam steam including Bob's Steam and 5dim
 for _, v in pairs(data.raw["generator"]) do
-	v.map_color = {r = 0.0, g = 0.35, b = 0.15} -- 0, 89, 38
+	v.friendly_map_color = {r = 0.0, g = 0.35, b = 0.15} -- 0, 89, 38
 end
 
 --changes color of radars
